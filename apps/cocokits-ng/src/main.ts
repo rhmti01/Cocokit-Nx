a7f69d82936f70188c486c3837cf929ae34cc47d
rhmti01 <rahmati.business@gmail.com>
2025-04-08 20:27:50 +0330

    config changes on cocokits/angular

--- apps/cocokits-ng/src/main.ts
+++ apps/cocokits-ng/src/main.ts
@@ -1,7 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideCocokits } from '@cocokits/angular-components';
+import { framesXThemeConfig } from '@cocokits/theme-frames-x';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideCocokits(framesXThemeConfig),
+  ]
+})
